4c47efec21af5a3a251e0609f4d03022e0d86515
Renzo <renzochong@gmail.com>
2023-10-07 20:01:55 -0400

    First Part of Practice Completed Where the app shows all images as a list of affirmation images.

---
+++ app/src/main/java/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
